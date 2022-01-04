32ec22adbe3a49ee19aca0e161c86c946911361b
Artur C. Miranda <artcamir12@gmail.com>
2022-01-04 16:00:35 -0300

    test3

--- contact/email.php
+++ contact/email.php
@@ -1,30 +1,31 @@
 <?php
 
-if(isset($_POST['email']) && !empty($_POST['email'])){
+phpinfo();
+// if(isset($_POST['email']) && !empty($_POST['email'])){
 
 
-$name = addslashes($_POST['name']);
-$email = addslashes($_POST['email']);
-$message = addslashes($_POST['message']);
+// $name = addslashes($_POST['name']);
+// $email = addslashes($_POST['email']);
+// $message = addslashes($_POST['message']);
 
-$to = "contato@sintecadoravalim.com";
-$subject = "Contato - Sintecadora Valim";
-$body = "Nome: ".$name. "\r\n".
-        "Email: ".$email. "\r\n".
-        "Mensagem: ".$message;
+// $to = "contato@sintecadoravalim.com";
+// $subject = "Contato - Sintecadora Valim";
+// $body = "Nome: ".$name. "\r\n".
+//         "Email: ".$email. "\r\n".
+//         "Mensagem: ".$message;
 
-$header = "From:contactform@sintecadoravalim.com". 
-"\r\n"."Reply-To:".$email."\r\n"."X-Mailer:PHP\".phpversion();
+// $header = "From:contactform@sintecadoravalim.com". 
+// "\r\n"."Reply-To:".$email."\r\n"."X-Mailer:PHP\".phpversion();
 
 
-if(mail($to, $subject, $body, $header)){
-    echo('Mensagem enviada');
-}
-else{
-    echo('A mensagem não pode ser enviada');
-}
+// if(mail($to, $subject, $body, $header)){
+//     echo('Mensagem enviada');
+// }
+// else{
+//     echo('A mensagem não pode ser enviada');
+// }
 
-}
+// }
 
 
 ?>
