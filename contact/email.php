c6c9f01c19a35a3f00a962be66b81699924cc207
Artur C. Miranda <artcamir12@gmail.com>
2022-01-04 16:27:43 -0300

    contact page in send email

--- contact/email.php
+++ contact/email.php
@@ -10,8 +10,8 @@ $message = $_POST['message'];
 $mailTo = "contato@sintecadoravalim.com";
 $headers = "From: ".$mailFrom;
 
-$txt = "Email recebido de ".$name.".\n\n".$message;
+$txt = "Email recebido de ".$name".\n\n".$message;
 
 mail($mailTo, $subject, $txt, $headers);
-header("Location: index.php?mailsend");
+header("Location: index.html");
 }
