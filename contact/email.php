6b3027c4e4051d92e63f82d301e9dbc614edec3e
Artur C. Miranda <artcamir12@gmail.com>
2022-01-04 15:24:15 -0300

    updated php email

--- contact/email.php
+++ contact/email.php
@@ -9,12 +9,11 @@ $message addslashes($_POST['message']);
 
 $to = "contato@sintecadoravalim.com";
 $subject = "Contato - Sintecadora Valim"
-$body = "Nome: ".$nome. "\r\n"
-        "Email: ".$email. "\r\n"
+$body = "Nome: ".$nome. "\r\n".
+        "Email: ".$email. "\r\n".
         "Mensagem: ".$message;
 
-$header = "From: contactform@sintecadoravalim.com". "\r\n"
-."Reply-To:".$email."\r\n"."X-Mailer:PHP\".phpversion();
+$header = "From: contactform@sintecadoravalim.com". "\r\n"."Reply-To:".$email."\r\n"."X-Mailer:PHP\".phpversion();
 
 
 if(mail($to, $subject, $body, $header)){
